147ae1063557409eed1cfe85c42ba6d6d0f5525b
Oscar Almgren <oscar.almgren@gmail.com>
2020-05-24 20:21:50 +0200

    statefulWidget setState()

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,56 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
         appBar: AppBar(
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        backgroundColor: Colors.lightBlueAccent,
+        body: SafeArea(
+          child: DicePage(),
+        ),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 2;
+  int rightDiceNumber = 3;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = ;
+                });
+                debugPrint('Left is:$leftDiceNumber');
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+              onPressed: () {
+                debugPrint('Other dice pressed!');
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
